be2c9c851170afe06fd7fc61754410cf324c3902
janessaperry <hello@janessaperry.com>
2024-11-19 15:32:17 -0800

    initial commit

---
+++ server.js
@@ -0,0 +1,11 @@
+import express, { json } from "express";
+import cors from "cors";
+import "dotenv/config";
+
+const PORT = process.env.PORT;
+
+const app = express();
+app.use(express.json());
+app.use(cors());
+
+app.listen(PORT, () => console.log(`app is listning on ${PORT}`));
