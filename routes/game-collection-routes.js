21d081bba39278de102e2ad0bab8f8fe3143f33f
janessaperry <hello@janessaperry.com>
2024-11-25 10:54:24 -0800

    add mock auth to game collection routes

--- routes/game-collection-routes.js
+++ routes/game-collection-routes.js
@@ -2,13 +2,28 @@ import express from "express";
 import * as gameCollectionController from "../controllers/game-collection-controller.js";
 const router = express.Router();
 
-router
-	.route("/collection/:userId")
-	.get(gameCollectionController.getGameCollection);
+function authenticateToken(req, res, next) {
+	const token = req.headers.authorization.split(" ")[1];
+	if (!token) {
+		return res.status(401).json({ message: "No access token provided" });
+	}
+
+	if (token === "mock-access-token") {
+		req.userId = 1;
+		return next();
+	}
+
+	return res.status(498).json({ message: "Invalid access token" });
+}
 
 router
-	.route("/collection/:userId/:gameId")
-	.patch(gameCollectionController.updateGame)
-	.delete(gameCollectionController.deleteGame);
+	.route("/collection")
+	.get(authenticateToken, gameCollectionController.getGameCollection);
+
+router
+	.route("/collection/:gameId")
+	.post(authenticateToken, gameCollectionController.addGame)
+	.patch(authenticateToken, gameCollectionController.updateGame)
+	.delete(authenticateToken, gameCollectionController.deleteGame);
 
 export default router;
