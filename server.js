93bd174de81c77af4ac84712f2c71e9825aa2eff
janessaperry <hello@janessaperry.com>
2024-11-25 15:09:49 -0800

    add search games routes and serve public folder for images

--- server.js
+++ server.js
@@ -3,13 +3,16 @@ import cors from "cors";
 import "dotenv/config";
 import gameCollectionRoutes from "./routes/game-collection-routes.js";
 import gameDetailsRoutes from "./routes/game-details-routes.js";
+import searchGamesRoutes from "./routes/search-games-routes.js";
 const PORT = process.env.PORT || 8080;
 
 const app = express();
+app.use(express.static("public"));
 app.use(express.json());
 app.use(cors());
 
 app.use("/api", gameCollectionRoutes);
 app.use("/api", gameDetailsRoutes);
+app.use("/api", searchGamesRoutes);
 
 app.listen(PORT, () => console.log(`app is listening on ${PORT}`));
