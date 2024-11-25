1c964e3c647f22cb4e8931b22a5e29cf9218dabb
janessaperry <hello@janessaperry.com>
2024-11-25 10:54:05 -0800

    add mock auth to game details routes

--- routes/game-details-routes.js
+++ routes/game-details-routes.js
@@ -4,6 +4,22 @@ import * as gameDetailsController from "../controllers/game-details-controller.j
 
 const router = express.Router();
 
-router.route("/game-details/:gameId").get(gameDetailsController.getGameDetails);
+function authenticateToken(req, res, next) {
+	const token = req.headers.authorization.split(" ")[1];
+	if (!token) {
+		return res.status(401).json({ message: "No access token provided" });
+	}
+
+	if (token === "mock-access-token") {
+		req.userId = 1;
+		return next();
+	}
+
+	return res.status(498).json({ message: "Invalid access token" });
+}
+
+router
+	.route("/game-details/:gameId")
+	.get(authenticateToken, gameDetailsController.getGameDetails);
 
 export default router;
