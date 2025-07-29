0f7848820a6f3604052ffc806e3bb0aa8e80c809
janessaperry <hello@janessaperry.com>
2025-07-29 14:55:21 -0700

    add migration for PasswordReset table with schema and foreign key definition

---
+++ prisma/migrations/20250729214203_init/migration.sql
@@ -0,0 +1,13 @@
+-- CreateTable
+CREATE TABLE "PasswordReset" (
+    "id" SERIAL NOT NULL,
+    "userId" INTEGER NOT NULL,
+    "passcode" TEXT NOT NULL,
+    "expiresAt" TIMESTAMP(3) NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "PasswordReset_pkey" PRIMARY KEY ("id")
+);
+
+-- AddForeignKey
+ALTER TABLE "PasswordReset" ADD CONSTRAINT "PasswordReset_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
