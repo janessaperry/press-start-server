0f69bf2d4b0584fe8e35432c8a45dec26f6df4cb
janessaperry <hello@janessaperry.com>
2025-08-19 14:14:17 -0700

    add `resend` package and .env.example for server configuration setup

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,7 @@
-// Libraries
 import express, { Request, Response } from 'express';
 import cors from 'cors';
 import 'dotenv/config';
 
-// Routes
 import authRoutes from "./routes/authRoutes.js";
 
 const app = express();
