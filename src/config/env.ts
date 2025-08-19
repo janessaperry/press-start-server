6ac3c303beeee8a1a587d9c8d279eb4cd69d4f11
janessaperry <hello@janessaperry.com>
2025-08-19 16:31:26 -0700

    update env config to include FRONTEND_URL and RESEND_KEY; refactor server to use ENV constants

--- src/config/env.ts
+++ src/config/env.ts
@@ -4,7 +4,10 @@ dotenv.config();
 
 export const ENV = {
   SERVER_PORT: process.env.SERVER_PORT || '3000',
+  CORS_ORIGIN: process.env.CORS_ORIGIN || '3000',
+  FRONTEND_URL: process.env.FRONTEND_URL || '',
   CLIENT_ID: process.env.CLIENT_ID || '',
   ACCESS_TOKEN: process.env.ACCESS_TOKEN || '',
-  JWT_SECRET: process.env.JWT_SECRET || ''
+  JWT_SECRET: process.env.JWT_SECRET || '',
+  RESEND_KEY: process.env.RESEND_KEY || ''
 };
--- src/server.ts
+++ src/server.ts
@@ -1,14 +1,15 @@
 import express, { Request, Response } from 'express';
 import cors from 'cors';
+
 import 'dotenv/config';
+import { ENV } from "./config/env.js";
 
 import authRoutes from "./routes/authRoutes.js";
 
 const app = express();
-const { SERVER_PORT, CORS_ORIGIN } = process.env;
 
 app.use(express.json());
-app.use(cors({ origin: CORS_ORIGIN }));
+app.use(cors({ origin: ENV.CORS_ORIGIN }));
 
 app.get('/', (req: Request, res: Response) => {
   res.json({ message: 'Welcome to the Press Start API!' });
@@ -21,7 +22,7 @@ app.get('/health', (req: Request, res: Response) => {
   res.status(200).json({ status: 'OK', timestamp: new Date().toISOString() });
 });
 
-const port = SERVER_PORT || 3000;
+const port = ENV.SERVER_PORT || 3000;
 app.listen(port, () => {
   console.log(`Server running at http://localhost:${port}`);
 });
